1e95582947fa90e9c48d5f63beb5a0d51c65e570
Leianne Louis <leilouis112@gmail.com>
2019-07-14 23:50:11 -0400

    first commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            SayHi("Leianne");
+            Console.WriteLine(Cubed(43));
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static int Cubed(int num)
+        {
+            int result;
+            result = (int)Math.Pow(num, 3);
+
+            return result;
+        }
+    }
+}
